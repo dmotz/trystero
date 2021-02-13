c220d76ba325ad81f33c6f4ad9aac7aad973991c
Dan Motzenbecker <dan@oxism.com>
2021-02-12 19:58:30 -0500

    strategy-agnostic room module

--- src/room.js
+++ src/room.js
@@ -1,173 +1,46 @@
-import firebase from '@firebase/app'
-import '@firebase/database'
-import Peer from 'simple-peer-light'
+import {
+  keys,
+  values,
+  entries,
+  mkErr,
+  noOp,
+  encodeBytes,
+  decodeBytes
+} from './utils'
 
-const libName = 'Trystero'
-const defaultRootPath = `__${libName.toLowerCase()}__`
-const presencePath = '_'
-const buffLowEvent = 'bufferedamountlow'
-const occupiedRooms = {}
-const {keys, values, entries} = Object
 const TypedArray = Object.getPrototypeOf(Uint8Array)
 const typeByteLimit = 12
 const metaTagSize = typeByteLimit + 2
 const chunkSize = 16 * 2 ** 10 - metaTagSize
-const charSet = new Array(62)
-  .fill()
-  .map((_, i) => String.fromCharCode(i + (i > 9 ? (i > 35 ? 61 : 55) : 48)))
-  .join('')
+const buffLowEvent = 'bufferedamountlow'
 
-const noOp = () => {}
-const mkErr = msg => new Error(`${libName}: ${msg}`)
-const getPath = (...xs) => xs.join('/')
-const encodeBytes = txt => new TextEncoder().encode(txt)
-const decodeBytes = txt => new TextDecoder().decode(txt)
-
-let didInit = false
-let db
-let rootPath
-
-export const selfId = new Array(32)
-  .fill()
-  .map(() => charSet[Math.floor(Math.random() * charSet.length)])
-  .join('')
-
-export function init(config) {
-  if (didInit) {
-    return
-  }
-
-  if (!firebase.apps.length) {
-    if (!config) {
-      throw mkErr('init() requires a config map as the first argument')
-    }
-
-    if (!config.dbUrl) {
-      throw mkErr('config map is missing dbUrl field')
-    }
-
-    firebase.initializeApp({databaseURL: config.dbUrl})
-  }
-
-  didInit = true
-  db = firebase.database()
-  rootPath = (config && config.rootPath) || defaultRootPath
-}
-
-export function joinRoom(ns, limit) {
-  if (!didInit) {
-    throw mkErr('must call init() before joining room')
-  }
-
-  if (!ns) {
-    throw mkErr('namespace argument required')
-  }
-
-  if (occupiedRooms[ns]) {
-    throw mkErr(`already joined room ${ns}`)
-  }
-
-  if (limit <= 0) {
-    throw mkErr('invalid limit value')
-  }
-
+export default (onPeer, onSelfLeave) => {
   const peerMap = {}
-  const peerSigs = {}
   const actions = {}
   const pendingTransmissions = {}
-  const roomRef = db.ref(getPath(rootPath, ns))
-  const selfRef = roomRef.child(selfId)
+  const exitPeer = id => {
+    if (!peerMap[id]) {
+      return
+    }
+    delete peerMap[id]
+    delete pendingTransmissions[id]
+    onPeerLeave(id)
+  }
 
-  let didSyncRoom = false
   let onPeerJoin = noOp
   let onPeerLeave = noOp
   let onPeerStream = noOp
   let selfStream
-  let limitRes
-  let limitRej
 
-  occupiedRooms[ns] = true
+  onPeer((peer, id) => {
+    peerMap[id] = peer
 
-  selfRef.set({[presencePath]: true})
-  selfRef.on('child_added', data => {
-    const peerId = data.key
-    if (peerId !== presencePath) {
-      data.ref.on('child_added', data => {
-        if (!(peerId in peerSigs)) {
-          peerSigs[peerId] = {}
-        }
-
-        if (data.key in peerSigs[peerId]) {
-          return
-        }
-
-        peerSigs[peerId][data.key] = true
-
-        let val
-
-        try {
-          val = JSON.parse(data.val())
-        } catch (e) {
-          console.error(`${libName}: received malformed SDP JSON`)
-        }
-
-        getPeer(peerId, false).connection.signal(val)
-        data.ref.remove()
-      })
-    }
-  })
-  selfRef.onDisconnect().remove()
-
-  roomRef.once('value', data => {
-    didSyncRoom = true
-
-    if (!limit) {
-      return
+    if (selfStream) {
+      peer.addStream(selfStream)
     }
 
-    if (keys(data.val()).length > limit) {
-      fns.leave()
-      limitRej(mkErr(`room ${ns} is full (limit: ${limit})`))
-    } else {
-      limitRes(fns)
-    }
-  })
-  roomRef.on('child_added', ({key}) => {
-    if (!didSyncRoom || key === selfId) {
-      return
-    }
-    getPeer(key, true)
-  })
-  roomRef.on('child_removed', data => exitPeer(data.key))
-
-  function getPeer(key, initiator) {
-    if (peerMap[key]) {
-      return peerMap[key]
-    }
-
-    let setReadiness
-    const peer = new Peer({initiator})
-    const obj = {
-      connection: peer,
-      whenReady: new Promise(res => (setReadiness = res))
-    }
-
-    peerMap[key] = obj
-
-    peer.on('signal', sdp => {
-      const ref = db.ref(getPath(rootPath, ns, key, selfId)).push()
-      ref.set(JSON.stringify(sdp))
-      ref.onDisconnect().remove()
-    })
-    peer.on('connect', () => {
-      setReadiness()
-      onPeerJoin(key)
-      if (selfStream) {
-        sendStream(obj, selfStream)
-      }
-    })
-    peer.on('close', () => exitPeer(key))
-    peer.on('stream', stream => onPeerStream(stream, key))
+    peer.on('close', () => exitPeer(id))
+    peer.on('stream', stream => onPeerStream(stream, id))
     peer.on('data', data => {
       const buffer = new Uint8Array(data)
       const action = decodeBytes(buffer.subarray(0, typeByteLimit))
@@ -183,18 +56,18 @@ export function joinRoom(ns, limit) {
         throw mkErr(`received message with unregistered type (${action})`)
       }
 
-      if (!pendingTransmissions[key]) {
-        pendingTransmissions[key] = {}
+      if (!pendingTransmissions[id]) {
+        pendingTransmissions[id] = {}
       }
 
-      if (!pendingTransmissions[key][action]) {
-        pendingTransmissions[key][action] = {}
+      if (!pendingTransmissions[id][action]) {
+        pendingTransmissions[id][action] = {}
       }
 
-      let target = pendingTransmissions[key][action][nonce]
+      let target = pendingTransmissions[id][action][nonce]
 
       if (!target) {
-        target = pendingTransmissions[key][action][nonce] = {chunks: []}
+        target = pendingTransmissions[id][action][nonce] = {chunks: []}
       }
 
       if (isMeta) {
@@ -213,13 +86,13 @@ export function joinRoom(ns, limit) {
       chunks.forEach((b, i) => full.set(b, i && chunks[i - 1].byteLength))
 
       if (isBinary) {
-        actions[action](full, key, target.meta)
+        actions[action](full, id, target.meta)
       } else {
         const text = decodeBytes(full)
-        actions[action](isJson ? JSON.parse(text) : text, key)
+        actions[action](isJson ? JSON.parse(text) : text, id)
       }
 
-      delete pendingTransmissions[key][action][nonce]
+      delete pendingTransmissions[id][action][nonce]
     })
     peer.on('error', e => {
       if (e.code === 'ERR_DATA_CHANNEL') {
@@ -228,163 +101,146 @@ export function joinRoom(ns, limit) {
       console.error(e)
     })
 
-    return obj
-  }
+    onPeerJoin(id)
+  })
 
-  function exitPeer(id) {
-    if (!peerMap[id]) {
-      return
-    }
-    delete peerMap[id]
-    delete pendingTransmissions[id]
-    onPeerLeave(id)
-  }
+  return {
+    makeAction: type => {
+      if (!type) {
+        throw mkErr('action type argument is required')
+      }
 
-  function makeAction(type) {
-    if (!type) {
-      throw mkErr('action type argument is required')
-    }
+      if (actions[type]) {
+        throw mkErr(`action '${type}' already registered`)
+      }
 
-    if (actions[type]) {
-      throw mkErr(`action '${type}' already registered`)
-    }
+      const typeEncoded = encodeBytes(type)
 
-    const typeEncoded = encodeBytes(type)
+      if (typeEncoded.byteLength > typeByteLimit) {
+        throw mkErr(
+          `action type string "${type}" (${typeEncoded.byteLength}b) exceeds ` +
+            `byte limit (${typeByteLimit}). Hint: choose a shorter name.`
+        )
+      }
 
-    if (typeEncoded.byteLength > typeByteLimit) {
-      throw mkErr(
-        `action type string "${type}" (${typeEncoded.byteLength}b) exceeds ` +
-          `byte limit (${typeByteLimit}). Hint: choose a shorter name.`
-      )
-    }
+      const typeBytes = new Uint8Array(typeByteLimit)
+      typeBytes.set(typeEncoded)
 
-    const typeBytes = new Uint8Array(typeByteLimit)
-    typeBytes.set(typeEncoded)
+      const typePadded = decodeBytes(typeBytes)
 
-    const typePadded = decodeBytes(typeBytes)
+      let nonce = 0
 
-    let nonce = 0
+      actions[typePadded] = noOp
+      pendingTransmissions[type] = {}
 
-    actions[typePadded] = noOp
-    pendingTransmissions[type] = {}
+      return [
+        async (data, peerId, meta) => {
+          const peers = entries(peerMap)
 
-    return [
-      async (data, peerId, meta) => {
-        const peers = entries(peerMap)
-
-        if (!peers.length) {
-          return
-        }
-
-        if (meta && typeof meta !== 'object') {
-          throw mkErr('action meta argument must be an object')
-        }
-
-        const isJson = typeof data === 'object' || typeof data === 'number'
-        const isBlob = data instanceof Blob
-        const isBinary =
-          isBlob || data instanceof ArrayBuffer || data instanceof TypedArray
-
-        if (meta && !isBinary) {
-          throw mkErr('action meta argument can only be used with binary data')
-        }
-
-        const buffer = isBinary
-          ? new Uint8Array(isBlob ? await data.arrayBuffer() : data)
-          : encodeBytes(isJson ? JSON.stringify(data) : data)
-
-        const metaEncoded = meta ? encodeBytes(JSON.stringify(meta)) : null
-
-        const chunkTotal =
-          Math.ceil(buffer.byteLength / chunkSize) + (meta ? 1 : 0)
-
-        const chunks = new Array(chunkTotal).fill().map((_, i) => {
-          const isLast = i === chunkTotal - 1
-          const isMeta = meta && i === 0
-          const chunk = new Uint8Array(
-            metaTagSize +
-              (isMeta
-                ? metaEncoded.byteLength
-                : isLast
-                ? buffer.byteLength - chunkSize * (chunkTotal - (meta ? 2 : 1))
-                : chunkSize)
-          )
-
-          chunk.set(typeBytes)
-          chunk.set([nonce], typeBytes.byteLength)
-          chunk.set(
-            [isLast | (isMeta << 1) | (isBinary << 2) | (isJson << 3)],
-            typeBytes.byteLength + 1
-          )
-          chunk.set(
-            meta
-              ? isMeta
-                ? metaEncoded
-                : buffer.subarray((i - 1) * chunkSize, i * chunkSize)
-              : buffer.subarray(i * chunkSize, (i + 1) * chunkSize),
-            metaTagSize
-          )
-
-          return chunk
-        })
-
-        nonce = (nonce + 1) & 0xff
-
-        const transmit = async ([id, peer]) => {
-          await peer.whenReady
-          const chan = peer.connection._channel
-          let chunkN = 0
-
-          while (chunkN < chunkTotal) {
-            if (chan.bufferedAmount > chan.bufferedAmountLowThreshold) {
-              await new Promise(res => {
-                const next = () => {
-                  chan.removeEventListener(buffLowEvent, next)
-                  res()
-                }
-                chan.addEventListener(buffLowEvent, next)
-              })
-            }
-
-            if (!peerMap[id]) {
-              break
-            }
-
-            peer.connection.send(chunks[chunkN++])
+          if (!peers.length) {
+            return
           }
-        }
 
-        if (peerId) {
-          const peer = peerMap[peerId]
-          if (!peer) {
-            throw mkErr(`no peer with id ${peerId} found`)
+          if (meta && typeof meta !== 'object') {
+            throw mkErr('action meta argument must be an object')
           }
-          return transmit([peerId, peer])
-        }
 
-        return Promise.all(peers.map(transmit))
-      },
-      f => (actions[typePadded] = f)
-    ]
-  }
+          const isJson = typeof data === 'object' || typeof data === 'number'
+          const isBlob = data instanceof Blob
+          const isBinary =
+            isBlob || data instanceof ArrayBuffer || data instanceof TypedArray
 
-  async function sendStream(peer, stream) {
-    await peer.whenReady
-    peer.connection.addStream(stream)
-  }
+          if (meta && !isBinary) {
+            throw mkErr(
+              'action meta argument can only be used with binary data'
+            )
+          }
 
-  const fns = {
-    makeAction,
+          const buffer = isBinary
+            ? new Uint8Array(isBlob ? await data.arrayBuffer() : data)
+            : encodeBytes(isJson ? JSON.stringify(data) : data)
+
+          const metaEncoded = meta ? encodeBytes(JSON.stringify(meta)) : null
+
+          const chunkTotal =
+            Math.ceil(buffer.byteLength / chunkSize) + (meta ? 1 : 0)
+
+          const chunks = new Array(chunkTotal).fill().map((_, i) => {
+            const isLast = i === chunkTotal - 1
+            const isMeta = meta && i === 0
+            const chunk = new Uint8Array(
+              metaTagSize +
+                (isMeta
+                  ? metaEncoded.byteLength
+                  : isLast
+                  ? buffer.byteLength -
+                    chunkSize * (chunkTotal - (meta ? 2 : 1))
+                  : chunkSize)
+            )
+
+            chunk.set(typeBytes)
+            chunk.set([nonce], typeBytes.byteLength)
+            chunk.set(
+              [isLast | (isMeta << 1) | (isBinary << 2) | (isJson << 3)],
+              typeBytes.byteLength + 1
+            )
+            chunk.set(
+              meta
+                ? isMeta
+                  ? metaEncoded
+                  : buffer.subarray((i - 1) * chunkSize, i * chunkSize)
+                : buffer.subarray(i * chunkSize, (i + 1) * chunkSize),
+              metaTagSize
+            )
+
+            return chunk
+          })
+
+          nonce = (nonce + 1) & 0xff
+
+          const transmit = async ([id, peer]) => {
+            const chan = peer._channel
+            let chunkN = 0
+
+            while (chunkN < chunkTotal) {
+              if (chan.bufferedAmount > chan.bufferedAmountLowThreshold) {
+                await new Promise(res => {
+                  const next = () => {
+                    chan.removeEventListener(buffLowEvent, next)
+                    res()
+                  }
+                  chan.addEventListener(buffLowEvent, next)
+                })
+              }
+
+              if (!peerMap[id]) {
+                break
+              }
+
+              peer.send(chunks[chunkN++])
+            }
+          }
+
+          if (peerId) {
+            const peer = peerMap[peerId]
+            if (!peer) {
+              throw mkErr(`no peer with id ${peerId} found`)
+            }
+            return transmit([peerId, peer])
+          }
+
+          return Promise.all(peers.map(transmit))
+        },
+        f => (actions[typePadded] = f)
+      ]
+    },
 
     leave: () => {
       entries(peerMap).forEach(([id, peer]) => {
-        peer.connection.destroy()
+        peer.destroy()
         delete peerMap[id]
       })
-      selfRef.off()
-      selfRef.remove()
-      roomRef.off()
-      delete occupiedRooms[ns]
+      onSelfLeave()
     },
 
     getPeers: () => keys(peerMap),
@@ -392,27 +248,32 @@ export function joinRoom(ns, limit) {
     addStream: (stream, peerId) => {
       if (typeof peerId === 'string') {
         const peer = peerMap[peerId]
+
         if (!peer) {
           throw mkErr(`no peer with id ${peerId} found`)
         }
-        sendStream(peer, stream)
+
+        peer.addStream(stream)
       } else {
         if (!peerId) {
           selfStream = stream
         }
-        values(peerMap).forEach(peer => sendStream(peer, stream))
+
+        values(peerMap).forEach(peer => peer.addStream(stream))
       }
     },
 
     removeStream: (stream, peerId) => {
       if (peerId) {
         const peer = peerMap[peerId]
+
         if (!peer) {
           throw mkErr(`no peer with id ${peerId} found`)
         }
-        peer.connection.removeStream(stream)
+
+        peer.removeStream(stream)
       } else {
-        values(peerMap).forEach(peer => peer.connection.removeStream(stream))
+        values(peerMap).forEach(peer => peer.removeStream(stream))
       }
     },
 
@@ -422,23 +283,4 @@ export function joinRoom(ns, limit) {
 
     onPeerStream: f => (onPeerStream = f)
   }
-
-  return limit
-    ? new Promise((res, rej) => {
-        limitRes = res
-        limitRej = rej
-      })
-    : fns
-}
-
-export function getOccupants(ns) {
-  if (!didInit) {
-    throw mkErr('must call init() before calling getOccupants()')
-  }
-
-  return new Promise(res =>
-    db
-      .ref(getPath(rootPath, ns))
-      .once('value', data => res(keys(data.val() || {})))
-  )
 }
