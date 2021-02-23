fde45b82b97c5416c11872474c28ea77787cd1e3
Dan Motzenbecker <dan@oxism.com>
2021-02-22 22:25:55 -0500

    added ipfs to bundle sizes script

--- bundle-sizes.sh
+++ bundle-sizes.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-for name in firebase torrent
+for name in torrent firebase ipfs
 do
   echo $name $(echo "import {joinRoom} from './src/$name'; joinRoom()" \
     | rollup --silent -p node-resolve | terser -cm | wc -c)
