f72e2220fbdb6a2352489d2efe5f7bcfe4503dd6
Dan Motzenbecker <dan@oxism.com>
2021-02-23 22:07:23 -0500

    defined internally usable makeAction

--- src/room.js
+++ src/room.js
@@ -7,7 +7,8 @@ import {
   noOp,
   encodeBytes,
   decodeBytes,
-  combineChunks
+  combineChunks,
+  libName
 } from './utils'
 
 const TypedArray = Object.getPrototypeOf(Uint8Array)
@@ -27,6 +28,133 @@ export default (onPeer, onSelfLeave) => {
     delete peerMap[id]
     delete pendingTransmissions[id]
     onPeerLeave(id)
+  }
+
+  const makeAction = type => {
+    if (!type) {
+      throw mkErr('action type argument is required')
+    }
+
+    if (actions[type]) {
+      throw mkErr(`action '${type}' already registered`)
+    }
+
+    const typeEncoded = encodeBytes(type)
+
+    if (typeEncoded.byteLength > typeByteLimit) {
+      throw mkErr(
+        `action type string "${type}" (${typeEncoded.byteLength}b) exceeds ` +
+          `byte limit (${typeByteLimit}). Hint: choose a shorter name.`
+      )
+    }
+
+    const typeBytes = new Uint8Array(typeByteLimit)
+    typeBytes.set(typeEncoded)
+
+    const typePadded = decodeBytes(typeBytes)
+
+    let nonce = 0
+
+    actions[typePadded] = noOp
+    pendingTransmissions[type] = {}
+
+    return [
+      async (data, peerId, meta) => {
+        const peers = entries(peerMap)
+
+        if (!peers.length) {
+          return
+        }
+
+        if (meta && typeof meta !== 'object') {
+          throw mkErr('action meta argument must be an object')
+        }
+
+        const isJson = typeof data === 'object' || typeof data === 'number'
+        const isBlob = data instanceof Blob
+        const isBinary =
+          isBlob || data instanceof ArrayBuffer || data instanceof TypedArray
+
+        if (meta && !isBinary) {
+          throw mkErr('action meta argument can only be used with binary data')
+        }
+
+        const buffer = isBinary
+          ? new Uint8Array(isBlob ? await data.arrayBuffer() : data)
+          : encodeBytes(isJson ? JSON.stringify(data) : data)
+
+        const metaEncoded = meta ? encodeBytes(JSON.stringify(meta)) : null
+
+        const chunkTotal =
+          Math.ceil(buffer.byteLength / chunkSize) + (meta ? 1 : 0)
+
+        const chunks = new Array(chunkTotal).fill().map((_, i) => {
+          const isLast = i === chunkTotal - 1
+          const isMeta = meta && i === 0
+          const chunk = new Uint8Array(
+            metaTagSize +
+              (isMeta
+                ? metaEncoded.byteLength
+                : isLast
+                ? buffer.byteLength - chunkSize * (chunkTotal - (meta ? 2 : 1))
+                : chunkSize)
+          )
+
+          chunk.set(typeBytes)
+          chunk.set([nonce], typeBytes.byteLength)
+          chunk.set(
+            [isLast | (isMeta << 1) | (isBinary << 2) | (isJson << 3)],
+            typeBytes.byteLength + 1
+          )
+          chunk.set(
+            meta
+              ? isMeta
+                ? metaEncoded
+                : buffer.subarray((i - 1) * chunkSize, i * chunkSize)
+              : buffer.subarray(i * chunkSize, (i + 1) * chunkSize),
+            metaTagSize
+          )
+
+          return chunk
+        })
+
+        nonce = (nonce + 1) & 0xff
+
+        const transmit = async ([id, peer]) => {
+          const chan = peer._channel
+          let chunkN = 0
+
+          while (chunkN < chunkTotal) {
+            if (chan.bufferedAmount > chan.bufferedAmountLowThreshold) {
+              await new Promise(res => {
+                const next = () => {
+                  chan.removeEventListener(buffLowEvent, next)
+                  res()
+                }
+                chan.addEventListener(buffLowEvent, next)
+              })
+            }
+
+            if (!peerMap[id]) {
+              break
+            }
+
+            peer.send(chunks[chunkN++])
+          }
+        }
+
+        if (peerId) {
+          const peer = peerMap[peerId]
+          if (!peer) {
+            throw mkErr(`no peer with id ${peerId} found`)
+          }
+          return transmit([peerId, peer])
+        }
+
+        return Promise.all(peers.map(transmit))
+      },
+      f => (actions[typePadded] = f)
+    ]
   }
 
   let onPeerJoin = noOp
@@ -108,135 +236,7 @@ export default (onPeer, onSelfLeave) => {
   })
 
   return {
-    makeAction: type => {
-      if (!type) {
-        throw mkErr('action type argument is required')
-      }
-
-      if (actions[type]) {
-        throw mkErr(`action '${type}' already registered`)
-      }
-
-      const typeEncoded = encodeBytes(type)
-
-      if (typeEncoded.byteLength > typeByteLimit) {
-        throw mkErr(
-          `action type string "${type}" (${typeEncoded.byteLength}b) exceeds ` +
-            `byte limit (${typeByteLimit}). Hint: choose a shorter name.`
-        )
-      }
-
-      const typeBytes = new Uint8Array(typeByteLimit)
-      typeBytes.set(typeEncoded)
-
-      const typePadded = decodeBytes(typeBytes)
-
-      let nonce = 0
-
-      actions[typePadded] = noOp
-      pendingTransmissions[type] = {}
-
-      return [
-        async (data, peerId, meta) => {
-          const peers = entries(peerMap)
-
-          if (!peers.length) {
-            return
-          }
-
-          if (meta && typeof meta !== 'object') {
-            throw mkErr('action meta argument must be an object')
-          }
-
-          const isJson = typeof data === 'object' || typeof data === 'number'
-          const isBlob = data instanceof Blob
-          const isBinary =
-            isBlob || data instanceof ArrayBuffer || data instanceof TypedArray
-
-          if (meta && !isBinary) {
-            throw mkErr(
-              'action meta argument can only be used with binary data'
-            )
-          }
-
-          const buffer = isBinary
-            ? new Uint8Array(isBlob ? await data.arrayBuffer() : data)
-            : encodeBytes(isJson ? JSON.stringify(data) : data)
-
-          const metaEncoded = meta ? encodeBytes(JSON.stringify(meta)) : null
-
-          const chunkTotal =
-            Math.ceil(buffer.byteLength / chunkSize) + (meta ? 1 : 0)
-
-          const chunks = new Array(chunkTotal).fill().map((_, i) => {
-            const isLast = i === chunkTotal - 1
-            const isMeta = meta && i === 0
-            const chunk = new Uint8Array(
-              metaTagSize +
-                (isMeta
-                  ? metaEncoded.byteLength
-                  : isLast
-                  ? buffer.byteLength -
-                    chunkSize * (chunkTotal - (meta ? 2 : 1))
-                  : chunkSize)
-            )
-
-            chunk.set(typeBytes)
-            chunk.set([nonce], typeBytes.byteLength)
-            chunk.set(
-              [isLast | (isMeta << 1) | (isBinary << 2) | (isJson << 3)],
-              typeBytes.byteLength + 1
-            )
-            chunk.set(
-              meta
-                ? isMeta
-                  ? metaEncoded
-                  : buffer.subarray((i - 1) * chunkSize, i * chunkSize)
-                : buffer.subarray(i * chunkSize, (i + 1) * chunkSize),
-              metaTagSize
-            )
-
-            return chunk
-          })
-
-          nonce = (nonce + 1) & 0xff
-
-          const transmit = async ([id, peer]) => {
-            const chan = peer._channel
-            let chunkN = 0
-
-            while (chunkN < chunkTotal) {
-              if (chan.bufferedAmount > chan.bufferedAmountLowThreshold) {
-                await new Promise(res => {
-                  const next = () => {
-                    chan.removeEventListener(buffLowEvent, next)
-                    res()
-                  }
-                  chan.addEventListener(buffLowEvent, next)
-                })
-              }
-
-              if (!peerMap[id]) {
-                break
-              }
-
-              peer.send(chunks[chunkN++])
-            }
-          }
-
-          if (peerId) {
-            const peer = peerMap[peerId]
-            if (!peer) {
-              throw mkErr(`no peer with id ${peerId} found`)
-            }
-            return transmit([peerId, peer])
-          }
-
-          return Promise.all(peers.map(transmit))
-        },
-        f => (actions[typePadded] = f)
-      ]
-    },
+    makeAction,
 
     leave: () => {
       entries(peerMap).forEach(([id, peer]) => {
