dab396aa527a0dfc862fb13e4204c509b84b7e4a
Dan Motzenbecker <dan@oxism.com>
2023-11-22 17:15:10 -0500

    playwright config

---
+++ playwright.config.js
@@ -0,0 +1,33 @@
+import {devices} from '@playwright/test'
+
+export default {
+  workers: 1,
+  timeout: 10000,
+  use: {
+    ignoreHTTPSErrors: true,
+    headless: true,
+    launchOptions: {
+      args: [
+        '--use-fake-ui-for-media-stream',
+        '--use-fake-device-for-media-stream',
+        '--disable-setuid-sandbox',
+        '--no-sandbox'
+      ],
+      firefoxUserPrefs: {
+        'media.navigator.permission.disabled': true,
+        'media.navigator.streams.fake': true
+      }
+    }
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: {...devices['Desktop Chrome']}
+    }
+  ],
+  webServer: {
+    command: 'http-server -S -C ./test/certs/cert.pem -K ./test/certs/key.pem',
+    url: 'https://localhost:8080/test',
+    ignoreHTTPSErrors: true
+  }
+}
