724b687f627da1ec802f4617b92e6f5fa1900667
Dan Motzenbecker <dan@oxism.com>
2023-11-25 11:44:57 -0500

    add firefox and webkit to tests

--- playwright.config.js
+++ playwright.config.js
@@ -23,6 +23,14 @@ export default {
     {
       name: 'chromium',
       use: {...devices['Desktop Chrome']}
+    },
+    {
+      name: 'firefox',
+      use: {...devices['Desktop Firefox']}
+    },
+    {
+      name: 'webkit',
+      use: {...devices['Desktop Safari']}
     }
   ],
   webServer: {
