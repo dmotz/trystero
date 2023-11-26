76b0ccf6b19e9b1b63b21d2388594480a9974ab8
Dan Motzenbecker <dan@oxism.com>
2023-11-26 14:05:27 -0500

    simplified bundle script

--- bundle.sh
+++ bundle.sh
@@ -3,6 +3,6 @@
 mkdir -p dist
 for name in torrent firebase ipfs
 do
-  rollup src/$name.js -p 'node-resolve={browser: true}' -p commonjs --format es \
-  | npx terser -cm --comments false > dist/trystero-$name.min.js
+  rollup src/$name.js -p 'node-resolve={browser: true}' -p commonjs --format es |
+  terser -cm --comments false > dist/trystero-$name.min.js
 done
