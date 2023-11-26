ee2b7bf8eb06bc77a5caaefb03af2f5a797699b6
Dan Motzenbecker <dan@oxism.com>
2023-11-25 21:52:50 -0500

    pass browser:true to bundler

--- bundle.sh
+++ bundle.sh
@@ -3,5 +3,6 @@
 mkdir -p dist
 for name in torrent firebase ipfs
 do
-  rollup src/$name.js -p node-resolve -p commonjs --format es | npx terser -cm --comments false > dist/trystero-$name.min.js
+  rollup src/$name.js -p 'node-resolve={browser: true}' -p commonjs --format es \
+  | npx terser -cm --comments false > dist/trystero-$name.min.js
 done
