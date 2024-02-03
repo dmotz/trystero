60daa8236b58dd1762376584f347d282b6bdccab
Dan Motzenbecker <dan@oxism.com>
2024-02-03 10:51:21 -0500

    added mqtt to bundle script

--- bundle.sh
+++ bundle.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 mkdir -p dist
-for name in torrent firebase ipfs
+
+for name in firebase ipfs mqtt torrent
 do
   rollup src/$name.js -p 'node-resolve={browser: true}' -p commonjs --format es |
   terser -cm --comments false > dist/trystero-$name.min.js
