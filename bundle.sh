0471108a9677c578107f9676d246ef9465b8c593
Dan Motzenbecker <dan@oxism.com>
2024-02-11 13:43:50 -0500

    removed bundle script

--- bundle.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-mkdir -p dist
-
-for name in firebase ipfs mqtt torrent
-do
-  rollup src/$name.js -p 'node-resolve={browser: true}' -p commonjs --format es |
-  terser -cm --comments false > dist/trystero-$name.min.js
-done
