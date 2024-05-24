fa3312f67f73aa0e0705adcff3946fde26f47f0f
Dan Motzenbecker <dan@oxism.com>
2024-05-24 15:25:02 -0400

    supabase test

---
+++ test/supabase.spec.js
@@ -0,0 +1,7 @@
+import runTests from './tests'
+
+runTests('supabase', {
+  appId: 'https://onwtasuradgznfrfjipo.supabase.co',
+  supabaseKey:
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9ud3Rhc3VyYWRnem5mcmZqaXBvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTY1NzIyNzgsImV4cCI6MjAzMjE0ODI3OH0.jxcMN2uSG1gPaT1qkheWK-JzWSYZUjYFPzJKdGbWsn4'
+})
