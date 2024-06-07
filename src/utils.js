4993803b3853b17f3d2f644d6b7e04f7b108cb81
Dan Motzenbecker <dan@oxism.com>
2024-06-07 17:36:27 -0400

    removed unused export

--- src/utils.js
+++ src/utils.js
@@ -13,7 +13,7 @@ export const all = Promise.all.bind(Promise)
 
 export const isBrowser = typeof window !== 'undefined'
 
-export const {entries, fromEntries, keys, values} = Object
+export const {entries, fromEntries, keys} = Object
 
 export const noOp = () => {}
 
