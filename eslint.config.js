f273cd0780ccc5db8823f3dc1695789a6c1f83f6
Dan Motzenbecker <dan@oxism.com>
2024-06-07 21:22:11 -0400

    added process to eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,6 @@ import globals from 'globals'
 import pluginJs from '@eslint/js'
 
 export default [
-  {languageOptions: {globals: globals.browser}},
+  {languageOptions: {globals: {...globals.browser, process: true}}},
   pluginJs.configs.recommended
 ]
