e242085b258c5117885b561d334d8ee81a05a32e
Dan Motzenbecker <dan@oxism.com>
2024-07-06 17:52:41 -0400

    fixed ipfs strategy

--- src/ipfs.js
+++ src/ipfs.js
@@ -5,59 +5,72 @@ import {
   waitForRemotePeer,
   Protocols
 } from '@waku/sdk'
+import {wakuPeerExchangeDiscovery} from '@waku/discovery'
 import strategy from './strategy.js'
-import {all, decodeBytes, encodeBytes, selfId, toJson} from './utils.js'
+import {
+  all,
+  decodeBytes,
+  encodeBytes,
+  libName,
+  selfId,
+  toJson
+} from './utils.js'
+
+const pubsubTopic = '/waku/2/default-waku/proto'
+
+const contentTopic = topic => `/${libName}/0/${topic}/json`
+
+const sendMessage = (node, topic, payload) =>
+  node.lightPush.send(
+    createEncoder({
+      pubsubTopic,
+      contentTopic: contentTopic(topic),
+      ephemeral: true
+    }),
+    {payload: encodeBytes(payload)}
+  )
 
 export const joinRoom = strategy({
   init: config =>
     createLightNode({
-      defaultBootstrap: true,
-      ...(config.libp2pConfig
-        ? {libp2p: config.libp2pConfig}
-        : {libp2p: {hideWebSocketInfo: true}})
+      defaultBootstrap: false,
+      pubsubTopics: [pubsubTopic],
+      bootstrapPeers: [
+        '/dns4/waku.myrandomdemos.online/tcp/8000/wss/p2p/16Uiu2HAmKfC2QUvMVyBsVjuEzdo1hVhRddZxo69YkBuXYvuZ83sc',
+        '/dns4/node-01.do-ams3.wakuv2.prod.status.im/tcp/8000/wss/p2p/16Uiu2HAmL5okWopX7NqZWBUKVqW8iUxCEmd5GMHLVPwCgzYzQv3e',
+        '/dns4/node-01.gc-us-central1-a.wakuv2.prod.statusim.net/tcp/8000/wss/p2p/16Uiu2HAmVkKntsECaYfefR1V2yCR79CegLATuTPE6B9TxgxBiiiA',
+        '/dns4/node-01.ac-cn-hongkong-c.wakuv2.prod.status.im/tcp/8000/wss/p2p/16Uiu2HAm4v86W3bmT1BiH6oSPzcsSr24iDQpSN5Qa992BCjjwgrD',
+        '/dns4/node-01.do-ams3.wakuv2.test.status.im/tcp/8000/wss/p2p/16Uiu2HAmPLe7Mzm8TsYUubgCAW1aJoeFScxrLj8ppHFivPo97bUZ'
+      ],
+      libp2p: {
+        peerDiscovery: [wakuPeerExchangeDiscovery([pubsubTopic])],
+        hideWebSocketInfo: true,
+        ...config.libp2pConfig
+      }
     }).then(async node => {
       await node.start()
       await waitForRemotePeer(node, [Protocols.LightPush, Protocols.Filter])
       return node
     }),
 
-  subscribe: async (node, rootTopic, selfTopic, onMessage) => {
-    const [rootSub, selfSub] = await all([
-      node.filter.createSubscription(),
-      node.filter.createSubscription()
-    ])
-
-    const rootDecoder = createDecoder(rootTopic)
-    const selfDecoder = createDecoder(selfTopic)
-
+  subscribe: (node, rootTopic, selfTopic, onMessage) => {
     const handleMsg = topic => msg => {
       if (msg.payload) {
         onMessage(topic, decodeBytes(msg.payload), (peerTopic, signal) =>
-          node.lightPush.send(
-            createEncoder({
-              contentTopic: peerTopic,
-              ephemeral: true
-            }),
-            {payload: encodeBytes(signal)}
-          )
+          sendMessage(node, peerTopic, signal)
         )
       }
     }
+    const rootDecoder = createDecoder(contentTopic(rootTopic), pubsubTopic)
+    const selfDecoder = createDecoder(contentTopic(selfTopic), pubsubTopic)
+    const unsubRoot = node.filter.subscribe(rootDecoder, handleMsg(rootTopic))
+    const unsubSelf = node.filter.subscribe(selfDecoder, handleMsg(selfTopic))
 
-    rootSub.subscribe([rootDecoder], handleMsg(rootTopic))
-    selfSub.subscribe([selfDecoder], handleMsg(selfTopic))
-
-    return () => {
-      rootSub.unsubscribe()
-      selfSub.unsubscribe()
-    }
+    return () => all([unsubRoot, unsubSelf]).then(fs => fs.forEach(f => f()))
   },
 
   announce: (node, rootTopic) =>
-    node.lightPush.send(
-      createEncoder({contentTopic: rootTopic, ephemeral: true}),
-      {payload: encodeBytes(toJson({peerId: selfId}))}
-    )
+    sendMessage(node, rootTopic, toJson({peerId: selfId}))
 })
 
 export {selfId} from './utils.js'
