bd3f5a22948c76edcebc37ebae8ab02ecfee4587
Dan Motzenbecker <dan@oxism.com>
2024-07-27 13:25:26 -0400

    use 0.20.0 on demo site

--- docs/site.js
+++ docs/site.js
@@ -1,4 +1,4 @@
-import {joinRoom, selfId} from 'https://esm.run/trystero@0.19.0'
+import {joinRoom, selfId} from 'https://esm.run/trystero@0.20.0'
 
 const byId = document.getElementById.bind(document)
 const canvas = byId('canvas')
