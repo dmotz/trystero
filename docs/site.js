23c7d8b41ada7bf5866bfdfb51c9fe2037f0d04b
Dan Motzenbecker <dan@oxism.com>
2025-05-25 15:09:09 -0400

    update site to 0.21.4

--- docs/site.js
+++ docs/site.js
@@ -1,4 +1,4 @@
-import {joinRoom, selfId} from 'https://esm.run/trystero@0.21.3'
+import {joinRoom, selfId} from 'https://esm.run/trystero@0.21.4'
 
 const byId = document.getElementById.bind(document)
 const canvas = byId('canvas')
