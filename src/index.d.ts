eff308779db7099ea7b5b178a2bcdf8358cdf838
Leonardo "Leu" Pereira <jlcvp@users.noreply.github.com>
2025-06-08 17:53:23 -0300

    Add TurnConfig typings for proper joinRoom configuration according to documentation

--- src/index.d.ts
+++ src/index.d.ts
@@ -45,6 +45,14 @@ declare module 'trystero' {
     relayRedundancy?: number
   }
 
+  export interface TurnConfig {
+    turnConfig?: {
+      urls: string | string[]
+      username?: string
+      credential?: string
+    }[]
+  }
+
   export interface Room {
     makeAction: <T extends DataPayload>(
       namespace: string
@@ -94,7 +102,7 @@ declare module 'trystero' {
   }
 
   export function joinRoom(
-    config: BaseRoomConfig & RelayConfig,
+    config: BaseRoomConfig & RelayConfig & TurnConfig,
     roomId: string
   ): Room
 
