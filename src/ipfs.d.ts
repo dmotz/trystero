2188f1c3433894ef7375147e616863eef501a22e
Dan Motzenbecker <dan@oxism.com>
2025-10-05 15:34:48 -0400

    ipfs: update type declaration

--- src/ipfs.d.ts
+++ src/ipfs.d.ts
@@ -1,15 +1,7 @@
 declare module 'trystero/ipfs' {
   import {BaseRoomConfig, Room} from 'trystero'
-  import type {Libp2pOptions} from 'libp2p'
 
-  export interface IpfsRoomConfig {
-    libp2pConfig?: Partial<Libp2pOptions>
-  }
-
-  export function joinRoom(
-    config: BaseRoomConfig & IpfsRoomConfig,
-    roomId: string
-  ): Room
+  export function joinRoom(config: BaseRoomConfig, roomId: string): Room
 
   export * from 'trystero'
 }
