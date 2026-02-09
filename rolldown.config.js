65163c7119ca66269258529d2afa11d75f84526b
Dan Motzenbecker <dan@oxism.com>
2026-02-09 13:48:25 -0500

    simplified rolldown

--- rolldown.config.js
+++ rolldown.config.js
@@ -1,18 +1,15 @@
 const config = {
-  output: {
-    format: 'es',
-    minify: !process.env.NO_MINIFY,
-    sourcemap: true,
-    codeSplitting: false
-  }
+  format: 'es',
+  minify: !process.env.NO_MINIFY,
+  sourcemap: true,
+  codeSplitting: false
 }
 
 export default ['firebase', 'ipfs', 'mqtt', 'nostr', 'supabase', 'torrent'].map(
   name => ({
-    ...config,
     input: `src/${name}.js`,
     output: {
-      ...config.output,
+      ...config,
       file: `dist/trystero-${name}.min.js`
     }
   })
